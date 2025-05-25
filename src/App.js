452937d6d4e4b7160ce5aba8aa371d2d41c9b78f
GenieTech07 <senzendragon@gmail.com>
2025-05-25 11:39:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px", fontSize: "24px" }}>
+      <h1>Welcome to KitchenGenie</h1>
+      <p>Launching soon on the Play Store. Stay tuned!</p>
+    </div>
+  );
+};
+
+export default App;
